323408b504fd294d76393e10b4f8ea3ed54cd4ac
renasami <drrr0502@gmail.com>
2021-12-21 17:41:49 +0900

    svelte todo fin

--- svelte-todo/src/App.svelte
+++ svelte-todo/src/App.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
 	import Task from "./Task.svelte"
-	let title = "a"
-	console.log(title)
+	import {taskList} from "./stores"
+	let text = ""
+	const addTask = () => {
+		if (!text) return
+		taskList.update((task) => [...task,{
+			id:task.length,
+			text:text,
+			done:false
+		}])
+		text = ""
+	}
+
+	const delteTask = () => {
+		const newTask = $taskList.filter((task) => !task.done)
+		console.log(newTask)
+		taskList.update(() => [...newTask])
+	}
+
 </script>
 
 <h1>svelte todo</h1>
 <hr />
 <div>
-	<input bind:value={title}/>
-	<button>追加</button>
+	<input bind:value={text}/>
+	<button on:click={addTask}>追加</button>
 	<ul>
-	<Task contents={title}/>
+	{#each $taskList as task }
+		<Task task={task}/>
+	{/each}
 	</ul>
-	<button>終了済みタスク削除</button>
+	<button on:click={delteTask}>終了済みタスク削除</button>
 </div>
 
--- svelte-todo/src/Task.svelte
+++ svelte-todo/src/Task.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-    export let contents = ""
+import type { Task } from "svelte/internal";
+
+    import {taskList} from "./stores"
+    import type {TaskType} from "./types"
+
+    export let task:TaskType
+    const handleOnClick = () => {
+        const newTask:TaskType[] = $taskList.map((t,i) => {
+            if (task.id !== i) return t
+            return {
+                id: t.id,
+                text: t.text,
+                done:!t.done
+            }
+        })
+        taskList.update(() => [...newTask])
+    }
 </script>
 
 <li>
-    <input type="checkbox">
-    <p>{contents}</p>
+    <input type="checkbox" checked={task.done} on:click={handleOnClick}>
+    <p>{task.text}</p>
 </li>
 
 <style>
@@ -12,7 +28,7 @@
         display:flex;
         margin:10px;
     }
-    
+
     p {
         margin:0;
         margin-left: 10px;
